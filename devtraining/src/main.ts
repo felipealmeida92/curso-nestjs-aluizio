a8a67e50f7ca208eed5be8d306c52b0b958afc0f
felipealmeida92 <felipe.si.almeida@gmail.com>
2025-06-14 14:11:18 -0300

    feat(dto): validation pipe

--- devtraining/src/main.ts
+++ devtraining/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, //ignora propriedades que não estão no DTO
+    forbidNonWhitelisted: true, // caso tenha uma propriedade indevida a requisição é recusada
+    transform: true, // transformar o tipo de dado ex. string -> number
+  }))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
